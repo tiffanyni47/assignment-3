e88810fca9a85a0ae6976964fd75dab9ad4ec929
tiffanyni47 <102348115+tiffanyni47@users.noreply.github.com>
2025-04-14 14:23:23 -0400

    Update Home.js with css file

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,28 +6,29 @@ The Home component is used to demonstrate the use of Link.
 import React, {Component} from 'react';
 import AccountBalance from './AccountBalance';
 import {Link} from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className="home-container">
         <img src="https://picsum.photos/200/200" alt="bank"/>
 
         <h1>Bank of React</h1>
 
-        <Link to="/userProfile">
+        <Link to="/userProfile" className="home-link">
           <button>User Profile</button>
         </Link>
         <br/>
-        <Link to="/login">
+        <Link to="/login" className="home-link">
           <button>Login</button>
         </Link>
         <br/>
-        <Link to="/credits">
+        <Link to="/credits" className="home-link">
           <button>Credits</button>
         </Link>
         <br/>
-        <Link to="/debits">
+        <Link to="/debits" className="home-link">
           <button>Debits</button>
         </Link>
         <br/><br/>
